4fc579d8582db5e999ef9dac3a9e8c3f6b340579
integralz <luckystay12@gmail.com>
2021-02-03 11:48:44 +0900

    main device_size text change

--- js/device_size.js
+++ js/device_size.js
@@ -2,9 +2,7 @@ const device_size = document.querySelector(".device_size");
 const device_size_video = document.querySelector(".device_size_video");
 const device_size_text = document.querySelector(".device_size_text");
 const device_size_controller = new ScrollMagic.Controller();
-const device_size_text1 = new TimelineMax()
-.fromTo(device_size_text, 3, {opacity: 1}, {opacity:0})
-.fromTo(device_size_text, 3, {opacity: 0}, {opacity:1});
+const device_size_text1 = TweenMax.fromTo(device_size_text, 3, {opacity: 0}, {opacity:1});
 
 const device_size_scene = new ScrollMagic.Scene({
     duration: "90%",
@@ -18,7 +16,7 @@ const device_size_scene = new ScrollMagic.Scene({
 });
 
 const device_size_text_scene1 = new ScrollMagic.Scene({
-    duration: "90%",
+    duration: "180%",
     triggerElement: device_size,
     triggerHook: 0.1
 })
@@ -38,10 +36,4 @@ device_size_scene.on("update", e =>{
     else{
         device_size_delay = 0;
     }
-    if(e.scrollPos < (0.5 * e.startPos + 0.5 * e.endPos)){
-        device_size_text.innerHTML = "두가지 크기, <br> Pro와 Pro Max";
-    } 
-    else{
-        device_size_text.innerHTML = "그리고,<br> 다양한 색상";
-    } 
 });
